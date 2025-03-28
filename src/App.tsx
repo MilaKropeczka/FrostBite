4c1b905f759d3ff2679154ccb394360da1f4c3d6
MilaKropeczka <milakropeczka@gmail.com>
2025-03-28 11:22:18 +0100

    refactor/Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import './App.css';
-import BackgroundGradient from './components/BackgroundGradient';
 import { SearchBar } from './features/SearchBar';
 import ProductSection from './features/ProductSection';
 import Categories from './features/Categories';
@@ -8,7 +7,6 @@ import Header from './features/Header';
 function App() {
 	return (
 		<div className='w-full min-h-screen flex flex-col items-center bg-gray-100'>
-			<BackgroundGradient />
 			<Header />
 			<SearchBar />
 			<Categories />
--- src/features/Header.tsx
+++ src/features/Header.tsx
@@ -1,9 +1,16 @@
+import BackgroundGradient from '../components/BackgroundGradient';
 import StyledTitle from '../components/UI/StylesTitle';
 
 export default function Header() {
 	return (
-		<header className='relative mt-10 z-10 w-full max-w-xl drop-shadow-xl flex justify-center text-center items-center'>
-			<StyledTitle mainText='Explore Our' highlightedText='Flavours' />
-		</header>
+		<>
+			<BackgroundGradient />
+			<header className='relative mt-10 z-10 w-full max-w-xl drop-shadow-xl flex justify-center text-center items-center'>
+				<StyledTitle
+					mainText='Explore Our'
+					highlightedText='Flavours'
+				/>
+			</header>
+		</>
 	);
 }
