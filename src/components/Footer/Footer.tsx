a317c818d6549ba6c9d546137a43e43e9616c742
MilaKropeczka <milakropeczka@gmail.com>
2025-06-30 18:53:53 +0200

    fix: style footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,12 +1,9 @@
 export default function Footer() {
 	return (
-		<>
-			<div className='absolute w-full h-16 bg-gradient-to-t from-pink-200 bottom-0' />
-			<footer className='relative w-full h-16 text-pink-900 text-center rounded-t-2xl flex items-center justify-center'>
-				<p className='text-sm font-medium'>
-					© {new Date().getFullYear()} FrostBite. All rights reserved.
-				</p>
-			</footer>
-		</>
+		<footer className='h-16 absolute bottom-0 left-0 right-0 bg-gradient-to-t from-pink-200/70  text-pink-900 text-center rounded-t-2xl flex items-center justify-center'>
+			<p className='text-sm font-medium'>
+				© {new Date().getFullYear()} FrostBite. All rights reserved.
+			</p>
+		</footer>
 	);
 }
