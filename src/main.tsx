12d38340fd4bb372a93b9c9c1c3eb6a781cdffd6
MilaKropeczka <milakropeczka@gmail.com>
2025-07-23 18:42:06 +0200

    feat/setup-msw-and-react-query

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,32 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-import { BrowserRouter } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-createRoot(document.getElementById('root')!).render(
-	<StrictMode>
-		<BrowserRouter basename='/FrostBite'>
-			<App />
-		</BrowserRouter>
-	</StrictMode>
-);
+async function enableMocksAndRenderApp() {
+	if (import.meta.env.DEV) {
+		const { worker } = await import('./mocks/browser.ts');
+		console.log(import.meta.env.DEV);
+		await worker.start({
+			serviceWorker: {
+				url: '/FrostBite/mockServiceWorker.js',
+			},
+		});
+	}
+
+	const queryClient = new QueryClient();
+
+	createRoot(document.getElementById('root')!).render(
+		<StrictMode>
+			{/* <BrowserRouter> */}
+			<BrowserRouter basename='/FrostBite'>
+				<QueryClientProvider client={queryClient}>
+					<App />
+				</QueryClientProvider>
+			</BrowserRouter>
+		</StrictMode>
+	);
+}
+
+enableMocksAndRenderApp();
