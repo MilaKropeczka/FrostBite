cecdc2956fe4635c8f74744b3f5d6f93ddcfc2e9
MilaKropeczka <milakropeczka@gmail.com>
2025-07-30 14:21:50 +0200

    feat: ProductSection

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -3,12 +3,12 @@ import { Header } from '@/components/Header/Header';
 import { Outlet } from 'react-router';
 import { CartDrawer } from '@/features/cart';
 import { ProductFilterProvider } from '@/contexts/ProductFilterContext';
-import Footer from '@/components/Footer/Footer';
+import { Footer } from '@/components/Footer/Footer';
 import { ProfileDrawer } from '@/features/profile/ProfileDrawer';
 
 export function MainLayout() {
 	return (
-		<div className='relative w-full min-h-screen flex flex-col items-center bg-gray-100 px-4'>
+		<div className='relative w-full min-h-screen flex flex-col items-center bg-gray-100'>
 			<Header />
 			<ProductFilterProvider>
 				<SearchBar />
