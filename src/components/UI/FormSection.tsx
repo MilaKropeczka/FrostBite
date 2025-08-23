8b912dcbc828e18e14a91d4348e5e77c26a8fd4f
MilaKropeczka <milakropeczka@gmail.com>
2025-08-23 16:54:05 +0200

    feat: FormSection component

---
+++ src/components/UI/FormSection.tsx
@@ -0,0 +1,35 @@
+import { SecondTitle } from '@/components/UI/SecondTitle';
+import { Button } from '@/components/UI/Button';
+
+type FormSectionProps = {
+	title: string;
+	onSubmit: () => void;
+	isSubmitting: boolean;
+	isValid: boolean;
+	children: React.ReactNode;
+};
+
+export function FormSection({
+	title,
+	onSubmit,
+	isSubmitting,
+	isValid,
+	children,
+}: FormSectionProps) {
+	return (
+		<section className='w-full max-w-2xl py-16 mb-12 p-6'>
+			<SecondTitle title={title} />
+			<div className='bg-white/80 shadow-lg p-6 rounded-xl'>
+				<form onSubmit={onSubmit} className='flex flex-col gap-2'>
+					{children}
+					<Button
+						type='submit'
+						className='font-semibold py-3'
+						disabled={!isValid || isSubmitting}>
+						{isSubmitting ? 'Saving...' : 'Save'}
+					</Button>
+				</form>
+			</div>
+		</section>
+	);
+}
