bc4ede62655c48b7f552a723c668cbd25189feca
MilaKropeczka <milakropeczka@gmail.com>
2025-08-25 20:31:28 +0200

    feat: useLoginMutation

---
+++ src/services/api/useLoginMutation.tsx
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import { useMutation, UseMutationOptions } from '@tanstack/react-query';
+
+type LoginParams = { email: string; password: string };
+type LoginResponse = { token: string };
+
+const login = async ({
+	email,
+	password,
+}: LoginParams): Promise<LoginResponse> => {
+	const response = await axios.post('/api/login', { email, password });
+	localStorage.setItem('token', response.data.token);
+	return response.data;
+};
+
+export const useLoginMutation = (
+	options?: UseMutationOptions<LoginResponse, Error, LoginParams>
+) => {
+	return useMutation<LoginResponse, Error, LoginParams>({
+		mutationFn: login,
+		...options,
+	});
+};
+
+export const getToken = () => localStorage.getItem('token');
